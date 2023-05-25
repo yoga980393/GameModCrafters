52d1a3b96cde063f17f33957f761a6c092c0f502
Dan <dan900306@gmail.com>
2023-05-25 12:29:02 +0800

    0523

---
+++ GameModCrafters/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using GameModCrafters.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameModCrafters.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            // 登入頁面
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("帳密登入成功");
+            }
+            // 處理登入邏輯
+            // 檢查使用者名稱和密碼，進行身份驗證等等
+            // 返回結果
+            return View();
+        }
+    }
+}
---
+++ GameModCrafters/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameModCrafters.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Key, Required, MaxLength(255), Display(Name = "電子郵件")]
+        public string Email { get; set; }
+
+        [Required, MaxLength(255), Display(Name = "用戶名")]
+        public string Username { get; set; }
+
+        [Required, MaxLength(255), Display(Name = "密碼")]
+        public string Password { get; set; }
+    }
+}
