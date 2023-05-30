cb6a7f7d115187a9dd9c863724ea9279ffe03786
Dan <dan900306@gmail.com>
2023-05-30 10:30:19 +0800

    合併帳戶

--- GameModCrafters/Controllers/AccountController.cs
+++ GameModCrafters/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using GameModCrafters.Data;
+using GameModCrafters.Models;
 using GameModCrafters.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace GameModCrafters.Controllers
 {
@@ -29,6 +31,31 @@ namespace GameModCrafters.Controllers
             // 檢查使用者名稱和密碼，進行身份驗證等等
             // 返回結果
             return View();
+        }
+        [HttpGet]
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterPage([Bind("Email, Username, Password1,Password2")] RegisterViewModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    Email = register.Email,
+                    Username = register.Username,
+                    Password = register.Password1,
+                };
+                //string password1 = Request.Form["password1"];
+                //string password2 = Request.Form["password2"];
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+            }
+            return View(register);
         }
     }
 }
