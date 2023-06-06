08d298babc0b9c66427683e27443697a72d4c27f
g87899 <apple8912141@gmail.com>
2023-06-06 15:55:59 +0800

    Migrations修改01

---
+++ GameModCrafters/Migrations/20230606035614_addTag.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameModCrafters.Migrations
+{
+    public partial class addTag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tags",
+                columns: new[] { "TagId", "TagName" },
+                values: new object[,]
+                {
+                    { "t001", "劇情" },
+                    { "t002", "數值" },
+                    { "t003", "武器" },
+                    { "t004", "道具" },
+                    { "t005", "地圖" },
+                    { "t006", "音樂" },
+                    { "t007", "美術" },
+                    { "t008", "程式" },
+                    { "t009", "其他" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t001");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t002");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t003");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t004");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t005");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t006");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t007");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t008");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "TagId",
+                keyValue: "t009");
+        }
+    }
+}
